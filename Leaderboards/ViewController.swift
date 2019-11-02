5937402be9bc280af54fa85a3c1032db0825fcb3
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2019-11-02 18:01:28 +0800

    Initial Commit

---
+++ Leaderboards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leaderboards
+//
+//  Created by JiaChen(: on 2/11/19.
+//  Copyright © 2019 SST Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
